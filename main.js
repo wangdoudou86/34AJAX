c762c1b381659e6d53f12e6c3fed8e6b82ebad98
krystal <892184375@qq.com>
2018-03-01 22:14:03 +0800

    简易封装AJAX

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-myButton.addEventListener('click',(e)=>{
+window.jQuery = function(nodeOrSelector){
+  let nodes = {}
+  nodes.addClass = function(){}
+  return nodes
+}
+window.jQuery.ajax = function(url,method,body,success,fail){
   let request = new XMLHttpRequest()
-  request.open('post','/xxx')//配置request
-  request.setRequestHeader('Content-Type','x-www-form-urlencoded')
-  request.send('我要设置第四部分')
+  request.open(method,url)//配置request
   request.onreadystatechange = ()=>{
     if(request.readyState === 4){
-      console.log('请求响应完毕了')
-      console.log(request.status)
-      console.log(request.statusText)
       if(request.status >= 200 && request.status < 300){
-        console.log('说明请求成功')
-        console.log(request.getAllResponseHeaders())
-        console.log(request.getResponseHeader('Content-Type'))
-        console.log(request.responseText)
-        let string = request.responseText
-        let object = window.JSON.parse(string)
-        //把符合JSON语法的字符串转换成JS对应的值，JSON.parse是浏览器提供的
-        console.log(typeof object)
-        console.log(object)
+       success.call(undefined,request.responseText)
+      }else if(request.status >= 400){
+        fail.call(undefined,request)
       }
     }
   }
+  request.send(body)
+}
+
+window.$ = window.jQuery
+
+myButton.addEventListener('click',(e)=>{
+  window.jQuery.ajax(
+   '/xxx',
+   'post',
+   'a=1&&b=2',
+   ()=>{console.log(1)},
+   ()=>{console.log(2)}
+  )
 })
