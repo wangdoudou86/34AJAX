a4b2254f07234c2054f1e50de49f7ef4865a2965
krystal <892184375@qq.com>
2018-03-02 23:34:51 +0800

    自己用promise封装ajax

--- main.js
+++ main.js
@@ -1,31 +1,61 @@
 window.jQuery = function(nodeOrSelector){
   let nodes = {}
   nodes.addClass = function(){}
+  nodes.html = function(){}
   return nodes
 }
-window.jQuery.ajax = function(url,method,body,success,fail){
-  let request = new XMLHttpRequest()
-  request.open(method,url)//配置request
-  request.onreadystatechange = ()=>{
-    if(request.readyState === 4){
-      if(request.status >= 200 && request.status < 300){
-       success.call(undefined,request.responseText)
-      }else if(request.status >= 400){
-        fail.call(undefined,request)
-      }
-    }
+window.$ = window.jQuery
+window.Promise = function(fn){
+  return {
+    then: function(){}
   }
-  request.send(body)
 }
 
-window.$ = window.jQuery
+window.jQuery.ajax = function({url,method,body,headers}){
+  
+  // let url = options.url
+  // let method = options.method
+  // let body = options.body
+  // let success = options.success
+  // let fail = options.fail
+  // let headers = options.headers
+  // let {url,method,body,success,fail,headers} = options
+  //这6句let可用ES6的解构赋值语法替换，就是最后一句，也可以直接就写在参数中
+  return new Promise(function(resolve,reject){
+
+    let request = new XMLHttpRequest()
+    request.open(method,url)//配置request
+    for(let key in headers){ //遍历对象中的属性
+      let value = headers[key]
+      request.setRequestHeader(key,value)
+    }
+    request.onreadystatechange = ()=>{
+      if(request.readyState === 4){
+        if(request.status >= 200 && request.status < 300){
+         resolve.call(undefined,request.responseText)
+        }else if(request.status >= 400){
+          reject.call(undefined,request)
+        }
+      }
+    }
+    request.send(body)
+  })
+
+}
+
+
 
 myButton.addEventListener('click',(e)=>{
-  window.jQuery.ajax(
-   '/xxx',
-   'post',
-   'a=1&&b=2',
-   ()=>{console.log(1)},
-   ()=>{console.log(2)}
-  )
+  let promise = window.jQuery.ajax(
+   {url:'/xxx',
+   method:'post',
+   headers:{
+     'Content-Type':'x-www-form-urlencoded',
+     'doudou':'18'
+    }   
+   })
+   promise.then(
+     (text)=>{console.log(text)},
+     (request)=>{console.log(request)}
+    )
 })
